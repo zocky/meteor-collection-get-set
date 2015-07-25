63669b8f6c3a99035bec0c007c62c9048ef6be5a
zocky <zocky@nospam.please>
2015-07-25 17:44:09 +0200

    add git url to package.js

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  git: "",
+  git: "https://github.com/zocky/meteor-collection-get-set.git",
   name: "zocky:collection-get-set",
   summary: 'Adds .get(query,path), .set(query,path,value) and .set(query,{path:value,...}) to Meteor.Collection objects.'
   version: "0.0.1"
@@ -7,8 +7,8 @@ Package.describe({
 
 
 Package.on_use(function (api) {
-
   api.versionsFrom("METEOR-CORE@0.9.0-atm");
-  api.use(['underscore'], ['server']);
-  api.add_files('collection-get-set.js', ['server','client']);
+  api.use(['underscore'], ['client', 'server']);
+  api.use('mongo', ['client', 'server']);
+  api.add_files('collection-get-set.js', ['client', 'server']);
 });
