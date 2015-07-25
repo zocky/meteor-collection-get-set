921573af1b3a1f1105d9ef7adc79735179f684cd
zocky <zocky@nospam.please>
2015-07-25 18:48:51 +0200

    fix package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   git: "https://github.com/zocky/meteor-collection-get-set.git",
   name: "zocky:collection-get-set",
-  summary: 'Adds .get(query,path), .set(query,path,value) and .set(query,{path:value,...}) to Meteor.Collection objects.'
+  summary: "Adds .get(query,path), .set(query,path,value) and .set(query,{path:value,...}) to Meteor.Collection objects.",
   version: "0.0.1"
 });
 
